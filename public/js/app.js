1f4398af2dba296192b2dd8d3b73eb1f55e0e234
Shun Yu Rao <shunyu.rao@gmail.com>
2016-08-04 17:21:56 -0400

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,47 @@
+﻿
+var config = {
+    apiKey: "AIzaSyCHVnGF3d_VLrRdP0mcZklVb72YGwGXhTc",
+    authDomain: "medpager.firebaseapp.com",
+    databaseURL: "https://medpager.firebaseio.com",
+    storageBucket: "firebase-medpager.appspot.com",
+};
+
+firebase.initializeApp(config);
+
+var app = angular.module("moc-admin", ["moc-admin.services", "firebase"])
+
+app.controller("mainCtrl", function($scope, $firebaseObject, $firebaseAuth) {
+    $scope.data = {};
+    var ref = firebase.database().ref();
+
+    $scope.authenticate = function(password) {
+        firebase.auth().signInWithEmailAndPassword("uo.pom.elective@gmail.com", password).then(function(data) {
+            $scope.auth = true;
+            $(".alert-success").fadeIn();
+            $scope.data = $firebaseObject(ref);
+        }, function(error) {
+            $(".alert-danger").fadeIn();
+        });
+    };
+
+    $scope.authenticate('POMelective15');
+
+    $scope.signOut = function() {
+        firebase.auth().signOut().then(function() {
+            $scope.auth = false;
+            $scope.data = {};
+            // Sign-out successful.
+        }, function(error) {
+            // An error happened.
+        });
+    };
+
+    $scope.delete = function(parent, key) {
+        delete parent[key];
+    };
+
+    $scope.save = function() {
+        $scope.data.$save();
+    };
+
+});
---
+++ public/js/services.js
@@ -0,0 +1,26 @@
+﻿
+var app = angular.module('moc-admin.services', []);
+
+app.factory('timeService', function() {
+    return {
+        parseUNIX: function(stamp) {
+            return Date.parse(stamp);
+        }
+    }
+});
+
+app.filter('objFilter', function() {
+    return function(items, search) {
+        var result = {};
+        if (search.query) {
+            angular.forEach(items, function(value, key) {
+                if (value[search.param].toUpperCase().indexOf(search.query.toUpperCase()) > -1) {
+                    result[key] = value;
+                }
+            });
+            return result;
+        } else {
+            return items;
+        }
+    }
+});
