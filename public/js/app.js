f63d371432ae1f11056f328a42ee5ba5f6f8bb09
Shun Yu Rao <shunyu.rao@gmail.com>
2017-02-02 22:50:29 -0500

    Year 2 release

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-﻿
+﻿var email = "uo.pom.elective@gmail.com";
 var config = {
     apiKey: "AIzaSyCHVnGF3d_VLrRdP0mcZklVb72YGwGXhTc",
     authDomain: "medpager.firebaseapp.com",
@@ -10,31 +10,54 @@ firebase.initializeApp(config);
 
 var app = angular.module("moc-admin", ["moc-admin.services", "firebase"])
 
-app.controller("mainCtrl", function($scope, $firebaseObject, $firebaseAuth) {
+app.controller("mainCtrl", function($scope, $firebaseObject, $firebaseAuth, dataService) {
     $scope.data = {};
     var ref = firebase.database().ref();
 
+    $scope.errormsg = null;
     $scope.authenticate = function(password) {
-        firebase.auth().signInWithEmailAndPassword("uo.pom.elective@gmail.com", password).then(function(data) {
-            $scope.auth = true;
-            $(".alert-success").fadeIn();
-            $scope.data = $firebaseObject(ref);
+        firebase.auth().signInWithEmailAndPassword(email, password).then(function(data) {
+            $scope.errormsg = null;
         }, function(error) {
-            $(".alert-danger").fadeIn();
+            $scope.errormsg = error.message;
+            $scope.$apply();
         });
     };
 
-    $scope.authenticate('POMelective15');
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (firebase.auth().currentUser && firebase.auth().currentUser.email == email) {
+            $scope.auth = true;
+        } else {
+            $scope.auth = false;
+            $scope.data = {};
+        }
+    });
 
     $scope.signOut = function() {
         firebase.auth().signOut().then(function() {
-            $scope.auth = false;
-            $scope.data = {};
             // Sign-out successful.
+            $scope.authPassword = null;
         }, function(error) {
             // An error happened.
         });
     };
+
+    function loadResponses() {
+        $scope.data.$loaded(function(data){
+            $scope.responses = dataService.parseResponses(data)
+            $scope.starred = dataService.parseStarred(data)
+            var file = "";
+            for (var i in $scope.responses) {
+                line = "";
+                for (var j in $scope.responses[i]) {
+                    line += $scope.responses[i][j] + ","
+                }
+                file += line.slice(0, -1) + "\n";
+            }            
+            exportCSV(file, "ok.csv");
+            
+        });
+    }
 
     $scope.delete = function(parent, key) {
         delete parent[key];
@@ -44,4 +67,21 @@ app.controller("mainCtrl", function($scope, $firebaseObject, $firebaseAuth) {
         $scope.data.$save();
     };
 
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (firebase.auth().currentUser && firebase.auth().currentUser.email == email) {
+            $scope.auth = true;
+            $scope.data = $firebaseObject(ref);
+            loadResponses();
+        } else {
+            $scope.auth = false;
+            $scope.data = {};
+        }
+    });
+
+    function exportCSV(text, name) {
+        var a = document.getElementById("export-csv");
+        var file = new Blob([text], {type: 'text/plain'});
+        a.href = URL.createObjectURL(file);
+        a.download = name;
+    }
 });
--- public/js/services.js
+++ public/js/services.js
@@ -9,13 +9,51 @@ app.factory('timeService', function() {
     }
 });
 
+app.factory('dataService', function() {
+    return {
+        parseResponses: function(data) {
+            var formatted = [];
+            var questions = data.answers;
+            for (q in questions) {
+                for (u in questions[q]) {
+                    formatted.push({
+                        question: q,
+                        sID: u,
+                        answer: questions[q][u].answer,
+                        followup: questions[q][u].choice,
+                        correct: questions[q][u].isCorrect,
+                        followupCorrect: questions[q][u].isChoiceCorrect,
+                        isDaily: questions[q][u].isDaily,
+                        time: (new Date(questions[q][u].time)).toString().substring(4, 24),
+                    });
+                }
+            }
+            return formatted;
+        },
+        parseStarred: function(data) {
+            var formatted = [];
+            for (var i in data.starred) {
+                var ins = {
+                    id: i,
+                    question: data.pagerQuestions[i].question,
+                    starred: data.starred[i]
+                };
+                formatted.push(ins);
+            }
+            return formatted;
+        }
+    }
+});
+
+
 app.filter('objFilter', function() {
     return function(items, search) {
         var result = {};
         if (search.query) {
+            result = [];
             angular.forEach(items, function(value, key) {
-                if (value[search.param].toUpperCase().indexOf(search.query.toUpperCase()) > -1) {
-                    result[key] = value;
+                if (value.toUpperCase().indexOf(search.query.toUpperCase()) > -1) {
+                    result.push(value);
                 }
             });
             return result;
